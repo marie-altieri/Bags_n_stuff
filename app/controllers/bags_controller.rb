a875cc1743b643d1576ada6beab122cc4a462eac
victor de spirlet <victordespirlet@gmail.com>
2022-11-24 11:07:42 +0100

    created the search bar to find bags and category

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -8,4 +8,13 @@ class BagsController < ApplicationController
     @bag = Bag.find(params[:id])
     @booking = Booking.new
   end
+
+  def index
+    if params[:query].present?
+      sql_query = "brand ILIKE :query OR category ILIKE :query"
+      @bags = Bag.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @bags = Bag.all
+    end
+  end
 end
