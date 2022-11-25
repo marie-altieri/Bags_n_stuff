9c36433fe3643e58906a421982d7ba4f0645d25e
victor de spirlet <victordespirlet@gmail.com>
2022-11-25 12:18:35 +0100

    reviews added

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   resources :bags do
-    resources :reviews, except: [:destroy]
+    resources :reviews, only: [:create]
     resources :bookings, except: [:destroy, :index]
   end
 
